2baeaa56f003c2d748cb0ff13d2dfb860274b18b
removedporn <86824510+removedporn@users.noreply.github.com>
2022-09-04 10:29:22 +0800

    Delete build_artifacts.sh

--- build_artifacts.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-NOW=$(date +"%Y-%m-%dT%H:%M:%S%:z")
-sed -e "s/\${time}/$NOW/" -e "s/\${release_time}/$NOW/" < Cobalt.json.example > target/Cobalt.json
-mv target/cobalt-2.0.jar target/Cobalt.jar
