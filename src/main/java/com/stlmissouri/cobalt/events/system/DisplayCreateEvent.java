8c4333bda925e63e91ba842382ca7e4f5664aa19
removedporn <86824510+removedporn@users.noreply.github.com>
2022-09-04 10:25:50 +0800

    Delete src/main directory

--- src/main/java/com/stlmissouri/cobalt/events/system/DisplayCreateEvent.java
+++
@@ -1,7 +0,0 @@
-package com.stlmissouri.cobalt.events.system;
-
-import com.darkmagician6.eventapi.events.callables.EventCancellable;
-
-public class DisplayCreateEvent extends EventCancellable {
-
-}
--- src/main/java/com/stlmissouri/cobalt/util/pringles/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * User: Stl
- * Date: 3/20/2014
- * Time: 7:55 PM
- * Use:  This package contains old "managers" made by Ramisme, if you'd like to replace them feel free
- */
-package com.stlmissouri.cobalt.util.pringles;
